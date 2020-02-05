926b40464f85a757cc83c4f26deca0284386ab52
Mirandatz <>
2020-02-05 19:52:58 -0300

    Added multi-label dataset emotions

---
+++ emotions/generate_mulan_xmls.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+DATASET_NAME = 'emotions'
+FOLD_COUNT = 10
+CLASS_COUNT = 6
+
+
+def main():
+    ughz = """<?xml version="1.0" encoding="utf-8"?>""" + '\n'
+    ughz += """<labels xmlns="http://mulan.sourceforge.net/labels">""" + '\n'
+    for class_nr in range(CLASS_COUNT):
+        ughz += f"""<label name="Class{class_nr}"></label>""" + '\n'
+    ughz += """</labels>"""
+
+    for fold_nr in range(FOLD_COUNT):
+        xml_path = Path.cwd() / 'folds' / str(fold_nr) / 'mulan.xml'
+        xml_path.write_text(ughz)
+
+
+if __name__ == '__main__':
+    main()
